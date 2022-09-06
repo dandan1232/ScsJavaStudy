9bf80dca64e1d0cd8fc18f4351026966f5bac5a2
dandan1232 <345652127@qq.com>
2022-09-06 10:37:20 +0800

    记录苏城院java程序设计课程内容220906

---
+++ src/main/java/online/danbao/ScsJavaStudy/ScsjavastudyApplication.java
@@ -0,0 +1,13 @@
+package online.danbao.ScsJavaStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScsjavastudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScsjavastudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/online/danbao/ScsJavaStudy/ScsjavastudyApplicationTests.java
@@ -0,0 +1,13 @@
+package online.danbao.ScsJavaStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScsjavastudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
