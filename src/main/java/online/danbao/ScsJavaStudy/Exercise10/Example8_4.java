d365dd9c2319880ae62dfeccc048f5163ebd8c8f
dandan1232 <345652127@qq.com>
2022-10-06 20:18:12 +0800

    键入了非数字字符

---
+++ src/main/java/online/danbao/ScsJavaStudy/Exercise10/Example8_4.java
@@ -0,0 +1,25 @@
+package online.danbao.ScsJavaStudy.Exercise10;
+
+/**
+ * @author: 蛋宝
+ * @date: 2022/10/6 20:16
+ * @description:
+ */
+public class Example8_4 {
+    public static void main(String args[]) {
+        double aver=0,sum=0,item=0;
+        boolean computable=true;
+        for(String s:args) {
+            try{ item=Double.parseDouble(s);
+                sum=sum+item;
+            }
+            catch(NumberFormatException e) {
+                System.out.println("您键入了非数字字符:"+e);
+                computable=false;
+            }
+        }
+        if(computable) {
+            System.out.println("sum="+sum);
+        }
+    }
+}
