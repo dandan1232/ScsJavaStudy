64d0c7b92bfcbf044b0b031fb35138fe18ea4ab2
dandan1232 <345652127@qq.com>
2022-11-15 14:23:25 +0800

    简单计算器

---
+++ src/main/java/online/danbao/ScsJavaStudy/Exercise11/Exercise11_3/Example9_8.java
@@ -0,0 +1,9 @@
+package online.danbao.ScsJavaStudy.Exercise11.Exercise11_3;
+
+public class Example9_8 {
+   public static void main(String args[]) {
+      WindowOperation win=new WindowOperation();
+      win.setBounds(100,100,390,360);
+      win.setTitle("简单计算器");
+   }
+}
