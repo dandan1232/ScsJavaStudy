bf4ae7d8784b4bc4fe8895adbd37a4cdd58b12b0
dandan1232 <345652127@qq.com>
2022-11-15 14:18:29 +0800

    （2）参照Example9_5、WindowBoxLayout设计一注册界面

---
+++ src/main/java/online/danbao/ScsJavaStudy/Exercise11/Exercise11_2/Example9_5.java
@@ -0,0 +1,9 @@
+package online.danbao.ScsJavaStudy.Exercise11.Exercise11_2;
+
+public class Example9_5 {
+   public static void main(String args[]) {
+      WindowBoxLayout win=new WindowBoxLayout();
+      win.setBounds(100,100,310,260);
+      win.setTitle("嵌套盒式布局容器");
+   }
+}
