d551a882734240ecbddc3c44429958da16033ca8
dandan1232 <345652127@qq.com>
2022-11-22 17:37:10 +0800

    使用MVC结构编写三角形对象，根据三条边计算三角形的面积

---
+++ src/main/java/online/danbao/ScsJavaStudy/Exercise13/Exercise13_3/Example10_15.java
@@ -0,0 +1,11 @@
+package online.danbao.ScsJavaStudy.Exercise13.Exercise13_3;
+
+//使用MVC结构编写三角形对象，根据三条边计算三角形的面积
+public class Example10_15 {
+   public static void main(String args[]){
+      MVCWindow win = new MVCWindow();
+      win.setTitle("使用MVC结构");
+      win.setBounds(100,100,420,260);
+   }
+}
+
