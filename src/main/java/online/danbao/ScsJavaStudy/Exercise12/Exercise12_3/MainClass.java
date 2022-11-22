ce7bef9f2493482231e8eddca0115081ac82d892
dandan1232 <345652127@qq.com>
2022-11-22 14:39:01 +0800

    获取computerJframe

---
+++ src/main/java/online/danbao/ScsJavaStudy/Exercise12/Exercise12_3/MainClass.java
@@ -0,0 +1,13 @@
+package online.danbao.ScsJavaStudy.Exercise12.Exercise12_3;
+
+/**
+ * @author: 蛋宝
+ * @date: 2022/11/22 14:23
+ * @description:
+ */
+public class MainClass {
+    public static void main(String[] args) {
+        ComputerJFrame frame;
+        frame = new ComputerJFrame("算术测试");
+    }
+}
