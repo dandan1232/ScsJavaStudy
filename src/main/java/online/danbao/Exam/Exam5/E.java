b1c88dc3bd8df47eb665ce5b44b48c1735933ae0
dandan1232 <345652127@qq.com>
2022-12-05 23:18:47 +0800

    Monkey类中有个构造方法：Monkey (String s)，并且有个public void speak()方法，在speak方法中输出“咿咿呀呀......”的信息。

---
+++ src/main/java/online/danbao/Exam/Exam5/E.java
@@ -0,0 +1,23 @@
+package online.danbao.Exam.Exam5;
+
+/**
+ * @author: 蛋宝
+ * @date: 2022/10/5 18:32
+ * @description:
+ * 在主类E的main方法中创建Monkey与People类的对象类测试这2个类的功能。
+ */
+public class E {
+    public static void main(String[] args)
+    {
+        //实例化对象Monkey
+        Monkey m = new Monkey("Exercise7");
+        m.speak();
+
+        //实例化对象people
+        People p = new People("Exercise7_3");
+        p.speak();
+        p.think();
+
+
+    }
+}
---
+++ src/main/java/online/danbao/Exam/Exam5/People.java
@@ -0,0 +1,30 @@
+package online.danbao.Exam.Exam5;
+
+/**
+ * @author: 蛋宝
+ * @date: 2022/10/5 18:32
+ * @description:
+ * People类是Monkey类的子类，在People类中重写方法speak(),在speak方法
+ * 中输出“小样的，不错嘛！会说话了！”的信息。
+ * 在People类中新增方法void think()，在think方法中输出“别说话！认真思考！”
+ * 的信息。
+ */
+public class People extends Monkey
+{
+    //构造方法
+    People(String s)
+    {
+        super(s);
+    }
+
+    //重写父类方法speak
+    @Override
+    public void speak()
+    {
+        System.out.println("小样的，不错嘛！会说话了！");
+    }
+    public void think()
+    {
+        System.out.println("别说话，认真思考！");
+    }
+}
